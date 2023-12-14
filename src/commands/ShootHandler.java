6bdf5e9b3f2cc6d648a6b3df4b838ca4e4013e6a
JgV <jose.guilherme.vieira@gmail.com>
2023-12-14 16:58:44 +0000

    Alteracao ShootHandler

--- src/commands/ShootHandler.java
+++ src/commands/ShootHandler.java
@@ -1,5 +1,30 @@
 package commands;
 
-public class ShootHandler {
+import Battleship.Battleship;
 
+import java.util.ArrayList;
+
+public class ShootHandler implements CommandHandler {
+
+    // Lista do player com os barcos
+    int row;
+    char charCol; //y
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    @Override
+    public void execute(Battleship.PlayerHandler playerHandler) {
+        String input = playerHandler.getMessage();
+        int col;
+        charCol = input.charAt(0);
+        col = charCol - 'A' + 1;
+
+        row = Integer.parseInt(input);
+
+        list.get(col).get(row);
+        if ((col == 'X' || col == ' ' || col == '*')) {
+            throw new IndexOutOfBoundsException("Row out of bounds");
+
+        }
+    }
 }
